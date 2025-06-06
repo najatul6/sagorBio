420afcbc81b25a5caa7e267071a6fd1ac06311a4
najatul6 <mdnajatulislam.develop@gmail.com>
2025-06-06 21:47:37 +0800

    Create all Section and add bio text or something else

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import bgImage from './assets/bg.jpg'
+import './App.css'
+import 'animate.css';
+
+function App() {
+
+  return (
+    <div className="FullBio">
+      <div className="bio-container">
+        <div className="bio-content">
+      <div className="background-container">
+        <img src={bgImage} alt="Background" className="background-image animate__animated animate__zoomIn" />
+      </div>
+        <h2 className='animate__animated animate__fadeInLeft animate__delay-0.1s'>➡️ Nᴀᴍᴇ :  🔽 MD SAGOR AHMED🔽</h2>
+        <h2 className='animate__animated animate__fadeInRight animate__delay-0.8s'> N‌ɪᴄᴋɴᴀᴍᴇ : 💚SAKIB💚🔽</h2>
+        <h2 className='animate__animated animate__fadeInDown'>  ➡️C‌ʟᴀss : 👍NOT AVAILABLE ✅</h2>
+        <h2 className='animate__animated animate__fadeInUp'>
+          ➡️ Cɪᴛʏ : RONGPUR        🔽
+        </h2>
+        <h2 className='animate__animated animate__zoomIn'>
+          ➡️ Wɪsʜ ᴍᴇ : 03 JULY 🎂          🔽
+        </h2>
+        <h2 className='animate__animated animate__flipInX'>
+          ➡️ Cᴏᴜɴᴛʀʏ : 𝐁𝐚𝐧𝐠𝐥𝐚𝐝𝐞𝐬𝐡 🇧🇩      🔽
+        </h2>
+        <h2 className='animate__animated animate__flipInY'>
+          ➡️ S‌ᴛᴀᴛᴜs : S‌ɪɴɢʟᴇ 😀             🔽
+        </h2>
+        <h2 className='animate__animated animate__lightSpeedInLeft'>
+          ➡️ 𝚁𝚎𝚕𝚒𝚐𝚒𝚘𝚗 : 𝙸𝚜𝚕𝚊𝚖 💕      🔽
+        </h2>
+        <h2 className='animate__animated animate__lightSpeedInRight'>
+          ➡️ Lᴀɴɢᴜᴀɢᴇ ɪ ᴋɴᴏᴡ :               🔽
+          𝐁𝐚𝐧𝐠𝐥𝐚 // 𝐄𝐧𝐠𝐥𝐢𝐬𝐡 ❤️           🔽
+        </h2>
+        <h2 className='animate__animated animate__rotateIn'>
+          ✅🔠🔤🔠🔤🔠✅
+        </h2>
+        <h2 className='animate__animated animate__rotateInDownLeft'>
+          ✅◦•●◉✿🅒🅞🅝🅣🅐🅒🅣 ✿◉●•◦✅
+        </h2>
+        <h2 className='animate__animated animate__rotateInDownRight'>
+          Telegram User name:- @sagorchoudhory🟥🟥🟥
+        </h2>
+        <h2 className='animate__animated animate__rotateInUpLeft'>
+          📱 Facebook :- NoT Available
+        </h2>
+        <h2 className='animate__animated animate__rotateInUpRight'>
+          ➡️ Telegram Channel OWNER OF :-<a href="https://t.me/All_in_updatee" target="_blank" rel="noopener noreferrer">
+            https://t.me/All_in_updatee  😃
+          </a>
+        </h2>
+        <h2 className='animate__animated animate__zoomInRight'>
+          ➡️ OWNER OF BOT        🔽
+        </h2>
+        <h2 className='animate__animated animate__zoomInUp'>
+          Coming Soon🔽
+          🔽
+        </h2>
+        <h2 className='animate__animated animate__jackInTheBox'>
+          🌟 𝙏hank You For Reading My Bio.☑️
+        </h2>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
